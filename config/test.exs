064868ac9d7081f0d0eedb348e447498642a825f
Rafael Carvalho <az.carta@gmail.com>
2025-08-09 10:39:35 -0300

    initial commit

---
+++ config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :live_data_feed, LiveDataFeedWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "B6ZiiNGVlfrRLcNJstZe8puyGe8YUx42BmF4MQTYV0lzbB6qZBabarhkZ0J9jybQ",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
