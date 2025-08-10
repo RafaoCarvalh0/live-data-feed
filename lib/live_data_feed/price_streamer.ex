93ecfc0985d86e56afe8021a28088217e0d43f80
Rafael Carvalho <az.carta@gmail.com>
2025-08-10 15:44:52 -0300

    feat: enhance price streamer to calculate and broadcast price changes with last known prices

--- lib/live_data_feed/price_streamer.ex
+++ lib/live_data_feed/price_streamer.ex
@@ -1,6 +1,8 @@
 defmodule LiveDataFeed.PriceStreamer do
   use GenServer
 
+  alias LiveDataFeed.Stock.StockService
+
   require Logger
 
   @interval_in_ms 2_000
@@ -19,14 +21,41 @@ defmodule LiveDataFeed.PriceStreamer do
   def handle_info(:update, %{stock_price_fetcher: stock_price_fetcher} = state) do
     prices = stock_price_fetcher.fetch_prices()
 
-    # TODO: add mnesia to store last value
+    last_stock_data_lookup =
+      case StockService.get_stocks_data() do
+        [_ | _] = data ->
+          data
+          |> Enum.group_by(& &1.symbol)
+          |> Enum.map(fn {symbol, [last | _]} -> {symbol, last} end)
+          |> Map.new()
+
+        _ ->
+          %{}
+      end
+
     Enum.each(prices, fn %{symbol: symbol, current_price: price, timestamp: ts, volume: vol} ->
+      price_in_cents = price * 100
+
+      last_price =
+        last_stock_data_lookup
+        |> Map.get(symbol, %{})
+        |> Map.get(:price, 0)
+
+      price_change = price_in_cents - last_price
+
+      price_change_percent =
+        if last_price != 0 do
+          (price_in_cents - last_price) / last_price * 100
+        else
+          0
+        end
+
       Phoenix.PubSub.broadcast(LiveDataFeed.PubSub, "stocks:#{symbol}", %{
         symbol: symbol,
-        current_price: price,
-        last_price: 0,
-        price_change: 0,
-        price_change_percent: 0,
+        current_price: price_in_cents,
+        last_price: last_price,
+        price_change: price_change,
+        price_change_percent: price_change_percent,
         timestamp: ts,
         volume: vol
       })
