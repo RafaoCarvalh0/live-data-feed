df6a5afb1a3e190a68fac48bbb3645afbd74eb9a
Rafael Carvalho <az.carta@gmail.com>
2025-08-11 01:52:47 -0300

    fix: add missing Dialyzer ignore entry for router.ex

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -1,4 +1,5 @@
 [
+  {"deps/phoenix/lib/phoenix/router.ex", "The pattern can never match the type."},
   {"test/support/conn_case.ex", :unknown_function},
   {"test/support/conn_case.ex", "Function ExUnit.Callbacks.__merge__/4 does not exist."},
   {"test/support/conn_case.ex", "Function ExUnit.Callbacks.__noop__/0 does not exist."},
