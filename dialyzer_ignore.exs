cd7ff7edf0044161ca64fa29dc718c37ad633b7b
Rafael Carvalho <az.carta@gmail.com>
2025-08-11 01:42:47 -0300

    fix: correct syntax in Dialyzer ignore entries for consistency

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -1,6 +1,6 @@
 [
-  {:no_return, {:_, :_, :_, _}, file: ~c"test/support/conn_case.ex"},
-  {:no_return, {:_, :_, :_, _}, file: ~c"test/support/data_case.ex"},
-  {:unknown_function, {:_, :_, :_, _}, file: ~c"test/support/conn_case.ex"},
-  {:unknown_function, {:_, :_, :_, _}, file: ~c"test/support/data_case.ex"}
+  {:no_return, {:_, :_, :_, :_}, file: ~c"test/support/conn_case.ex"},
+  {:no_return, {:_, :_, :_, :_}, file: ~c"test/support/data_case.ex"},
+  {:unknown_function, {:_, :_, :_, :_}, file: ~c"test/support/conn_case.ex"},
+  {:unknown_function, {:_, :_, :_, :_}, file: ~c"test/support/data_case.ex"}
 ]
