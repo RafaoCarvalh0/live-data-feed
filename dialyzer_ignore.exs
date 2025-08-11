8adc2cdff777854ae5533b9e631cbe8194445ffc
Rafael Carvalho <az.carta@gmail.com>
2025-08-11 01:42:47 -0300

    fix: correct syntax in Dialyzer ignore entries for consistency

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -1,6 +1,12 @@
 [
-  {:no_return, {:_, :_, :_, _}, file: ~c"test/support/conn_case.ex"},
-  {:no_return, {:_, :_, :_, _}, file: ~c"test/support/data_case.ex"},
-  {:unknown_function, {:_, :_, :_, _}, file: ~c"test/support/conn_case.ex"},
-  {:unknown_function, {:_, :_, :_, _}, file: ~c"test/support/data_case.ex"}
+  {"test/support/conn_case.ex", :unknown_function},
+  {"test/support/conn_case.ex", "Function ExUnit.Callbacks.__merge__/4 does not exist."},
+  {"test/support/conn_case.ex", "Function ExUnit.Callbacks.__noop__/0 does not exist."},
+  {"test/support/conn_case.ex", "Function ExUnit.CaseTemplate.__proxy__/2 does not exist."},
+  {"test/support/data_case.ex", :unknown_function},
+  {"test/support/data_case.ex", "Function ExUnit.Callbacks.__merge__/4 does not exist."},
+  {"test/support/data_case.ex", "Function ExUnit.Callbacks.__noop__/0 does not exist."},
+  {"test/support/data_case.ex", "Function ExUnit.CaseTemplate.__proxy__/2 does not exist."},
+  {"test/support/data_case.ex", "The pattern can never match the type."},
+  {"test/support/conn_case.ex", "The pattern can never match the type."}
 ]
